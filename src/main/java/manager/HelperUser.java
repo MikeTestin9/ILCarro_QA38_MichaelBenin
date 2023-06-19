c49c44af01a5a500faa7b4a6ab1785234337859d
MikeTestin9 <Michael_benin@hotmail.com>
2023-06-19 16:41:45 +0300

    HW7

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,6 +1,9 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+
 
 public class HelperUser extends HelperBase{
 
@@ -9,5 +12,19 @@ public class HelperUser extends HelperBase{
         super(wd);
     }
 
+    public void openLoginForm() {
+        wd.findElement(By.xpath("//a[starts-with(@href,'/login')]")).click();
+
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.xpath("//input[1]"), email);
+        type(By.xpath("//*[@id='password']"), password);
+    }
+
+    public void submitLogin(){
+
+        click(By.cssSelector("button[type='submit']"));
+    }
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,10 +1,26 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
     public void loginPositive(){
+        String email = "mikeben@gmail.com", password = "Mb12345$";
+        app.getUser().openLoginForm();       //open login form
+        app.getUser().fillLoginForm(email, password);   //fill login form
+        app.getUser().submitLogin();     //click on button Yalla
+        app.getUser().pause(3000);   //pause for 3 seconds
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button[normalize-space()='Ok']")));
 
     }
-
+//    @Test
+//    public void loginPositiveTestBase(){
+//        String email = "mb@gmail.com", password = "Mb12345$";
+//        app.getUser().openLoginForm();       //open login form
+//        app.getUser().fillLoginForm(email, password);   //fill login form
+//        app.getUser().submitLogin();     //click on button Login
+//        app.getUser().pause(3000);   //pause for 3 seconds
+////        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+//        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//button")));
 }
